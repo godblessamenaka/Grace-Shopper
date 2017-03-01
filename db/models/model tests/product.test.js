8762076f00a9200776c49d97892c4e4f210235bd
Drew Slattery <drewslatts@gmail.com>
2017-03-01 13:33:13 -0500

    tests(productmodel):validation tests complete

--- db/models/model tests/product.test.js
+++ db/models/model tests/product.test.js
@@ -32,4 +32,59 @@ describe('Product', () => {
       })
     )
   })
+
+  describe('Validations', () => {
+    it('repaces image with placeholder if none supplied', () =>
+      Product.create({
+        name: 'test product',
+        description: 'we definitely remembered to write tests',
+        price: 120.25,
+        inventory: 189
+      })
+      .then(product => {
+        expect(product.image).to.equal('default.jpg')
+      })
+    );
+    it('sets inventory to 0 if none supplied', () =>
+      Product.create({
+        name: 'test product',
+        description: 'we definitely remembered to write tests',
+        price: 120.25,
+      })
+      .then(product => {
+        expect(product.inventory).to.equal(0)
+      })
+    )
+    it('requires a name', () => {
+      let product = Product.build({
+        description: 'we definitely remembered to write tests',
+        price: 120.25,
+      })
+      return product.validate()
+        .then(result => {
+          expect(result).to.be.an.instanceOf(Error);
+        })
+    })
+    it('requires a description', () => {
+      let product = Product.build({
+        name: 'testosterone potion',
+        price: 120.25,
+      })
+      return product.validate()
+        .then(result => {
+          expect(result).to.be.an.instanceOf(Error);
+        })
+    })
+    it('requires a price', () => {
+      let product = Product.build({
+        name: 'testosterone potion',
+        description: 'we definitely remembered to write tests',
+      })
+      return product.validate()
+        .then(result => {
+          expect(result).to.be.an.instanceOf(Error);
+        })
+    })
+  })
+
 })
