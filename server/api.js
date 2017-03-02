e732d5df7450d02ce1f3dd009312eb0ee39eafde
Menaka Sampath <menakasampath@gmail.com>
2017-03-02 11:30:01 -0500

    feat(routes): product and category get routes complete

--- server/api.js
+++ server/api.js
@@ -9,6 +9,7 @@ api
   .use('/users', require('./users'))
   .use('/orders', require('./orders'))
   .use('/reviews', require('./reviews'))
+  .use('/products', require('./products'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
---
+++ server/categories.js
@@ -0,0 +1,12 @@
+const db = require('APP/db')
+const Category = db.model('categories')
+
+module.exports = require('express').Router()
+
+.get('/', (req, res, next) =>
+    Category.findAll()
+    .then(categories => res.json(categories))
+    .catch(next))
+
+
+
