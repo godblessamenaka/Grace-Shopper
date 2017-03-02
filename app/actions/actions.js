78f8e9238034d1e44a1337b3b5a1c829cefb1cbc
Jessica Perina <jperina@gmail.com>
2017-03-02 13:02:22 -0500

    refactor(action directory): changed directory name for clarity

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -11,17 +11,6 @@ export const createUser = (user) => ({ type: CREATE_USER, userToCreate: user});
 export const deleteUser = (user) => ({ type: DELETE_USER, userToDelete: user});
 export const updateUser = (user) => ({ type: UPDATE_USER, userToUpdate: user});
 
-// Products
-export const GET_PRODUCTS = 'GET_PRODUCTS';
-export const GET_PRODUCT = 'GET_PRODUCT';
-export const CREATE_PRODUCT = 'CREATE_PRODUCT';
-export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
-
-export const getProducts = (products) => ({ type: GET_PRODUCTS, receivedProducts: products});
-export const getProduct = (product) => ({ type: GET_PRODUCT, receivedProduct: product});
-export const createProduct = (product) => ({ type: CREATE_PRODUCT, productToCreate: product});
-export const updateProduct = (product) => ({ type: UPDATE_PRODUCT, productToUpdate: product});
-
 // Reviews
 export const GET_REVIEWS = 'GET_REVIEWS';
 export const CREATE_REVIEWS = 'CREATE_REVIEWS';
