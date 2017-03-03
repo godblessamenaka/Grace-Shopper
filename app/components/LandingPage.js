f7cfe57bfbed474efd16c464168d177c5a8f31d4
Jessica Perina <jperina@gmail.com>
2017-03-03 16:01:02 -0500

    feat(landing page): complets landing page, maybe

---
+++ app/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router'
+import Search from '../components/Search'
+
+function LandingPage (props) {
+
+  return (
+    <div>
+      <Search handleChange={this.handleChange} inputValue={inputValue} />
+    </div>
+  )
+}
+
+export default LandingPage
