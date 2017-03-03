1e6fcf83a8f01ec8eb29915ecb24bba4348768b3
Drew Slattery <drewslatts@gmail.com>
2017-03-03 13:31:09 -0500

    refactor(cartreducer):removed inner object,

--- app/reducers/cart.jsx
+++ app/reducers/cart.jsx
@@ -1,19 +1,14 @@
 import { GET_CART, UPDATE_CART} from '../actions/carts'
 
-const initialState = {
-  cart: [],
-}
+const initialState = [];
 
 const cartReducer = (state = initialState, action) => {
-  const newState = Object.assign({}, state)
 
   switch (action.type) {
     case GET_CART:
-      newState.cart = action.receivedCart
-      break;
+      return action.receivedCart
     case UPDATE_CART:
-      newState.cart = [newState, ...newState.cart]
-      break;
+      return [state, ...action.payload]
     default:
       return state
   }
