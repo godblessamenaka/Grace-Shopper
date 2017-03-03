e947c0dcdab35affb6be55760c31c36f9d447615
Jessica Perina <jperina@gmail.com>
2017-03-03 16:01:32 -0500

    feat(landing page): adds landing page container with search

---
+++ app/containers/LandingPageContainer.js
@@ -0,0 +1,42 @@
+import LandingPage from '../components/LandingPage'
+import MultipleProducts from '../components/MultipleProducts'
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state) => {
+  const products = state.products
+
+  return {
+    products: products.allProducts
+  }
+}
+
+class LandingPageContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputValue: ''}
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    const inputValue = evt.target.value
+    this.setState({ inputValue })
+  }
+
+  render() {
+    const inputValue = this.state.inputValue
+    const filteredProducts = this.props.products.filter(product => product.name.match(inputValue))
+
+    return (
+      <div>
+        <LandingPage handleChange={this.state.handleChange} />
+        <MultipleProducts products={filteredProducts} />
+      </div>
+    );
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(LandingPageContainer)
+
