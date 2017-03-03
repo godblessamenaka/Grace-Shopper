a5f61555795b84b1a3f2a8815927726c4a4e2aa7
Jessica Perina <jperina@gmail.com>
2017-03-03 18:42:35 -0500

    feat(orders redux): adds dispatchers

--- app/actions/orders.js
+++ app/actions/orders.js
@@ -1,11 +1,46 @@
+import axios from 'axios';
+
+/* -----------------    ACTIONS     ------------------ */
 export const GET_ORDERS = 'GET_ORDERS';
 export const GET_ORDER = 'GET_ORDER';
 export const CREATE_ORDER = 'CREATE_ORDER';
 export const DELETE_ORDER = 'DELETE_ORDER';
 export const UPDATE_ORDER = 'UPDATE_ORDER';
 
-export const getOrders = (orders) => ({ type: GET_ORDERS, receivedOrders: orders});
-export const getOrder = (order) => ({ type: GET_ORDER, receivedOrder: order});
-export const createOrder = (order) => ({ type: CREATE_ORDER, orderToCreate: order});
-export const deleteOrder = (order) => ({ type: DELETE_ORDER, orderToDelete: order});
-export const updateOrder = (order) => ({ type: UPDATE_ORDER, orderToUpdate: order});
+/* ------------   ACTION CREATORS     ------------------ */
+const getOrders = (orders) => ({ type: GET_ORDERS, receivedOrders: orders});
+const getOrder = (order) => ({ type: GET_ORDER, receivedOrder: order});
+const createOrder = (order) => ({ type: CREATE_ORDER, orderToCreate: order});
+const deleteOrder = (order) => ({ type: DELETE_ORDER, orderToDelete: order});
+const updateOrder = (order) => ({ type: UPDATE_ORDER, orderToUpdate: order});
+
+/* ------------       DISPATCHERS     ------------------ */
+export const fetchOrders = () => dispatch => {
+        axios.get('/api/orders')
+        .then(res => dispatch(getOrders(res.data)))
+        .catch(err => console.error('Fetching orders unsuccessful', err))
+}
+
+export const fetchOrder = (id) => dispatch => {
+    axios.get(`/api/order/${id}`)
+        .then(res => dispatch(getOrder(res.data)))
+        .catch(err => console.error('Fetching order unsuccessful', err))
+}
+
+export const addOrder = order => dispatch => {
+    axios.post('/api/orders', order)
+        .then(res => dispatch(createOrder(res.data)))
+        .catch(err => console.error(`Creating order: ${order} unsuccessful`, err))
+}
+
+export const changeOrder = (id, order) => dispatch => {
+    axios.put(`/api/orders/${id}`, order)
+        .then(res => dispatch(updateOrder(res.data)))
+        .catch(err => console.error(`Updating order: ${order} unsuccessful`, err))
+}
+
+export const removeOrder = id => dispatch => {
+  dispatch(deleteOrder(id));
+  axios.delete(`/api/stories/${id}`)
+       .catch(err => console.error(`Removing order: ${id} unsuccessful`, err));
+}
