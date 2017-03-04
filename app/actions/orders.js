3fcb755cb24ef3f786e4b5313380e0a5981a110c
Jessica Perina <jperina@gmail.com>
2017-03-03 19:06:18 -0500

    refactor(actions): refactors dispatchers

--- app/actions/orders.js
+++ app/actions/orders.js
@@ -8,39 +8,39 @@ export const DELETE_ORDER = 'DELETE_ORDER';
 export const UPDATE_ORDER = 'UPDATE_ORDER';
 
 /* ------------   ACTION CREATORS     ------------------ */
-const getOrders = (orders) => ({ type: GET_ORDERS, receivedOrders: orders});
-const getOrder = (order) => ({ type: GET_ORDER, receivedOrder: order});
-const createOrder = (order) => ({ type: CREATE_ORDER, orderToCreate: order});
-const deleteOrder = (order) => ({ type: DELETE_ORDER, orderToDelete: order});
-const updateOrder = (order) => ({ type: UPDATE_ORDER, orderToUpdate: order});
+const getOrders = (orders) => ({ type: GET_ORDERS, receivedOrders: orders });
+const getOrder = (order) => ({ type: GET_ORDER, receivedOrder: order });
+const createOrder = (order) => ({ type: CREATE_ORDER, orderToCreate: order });
+const deleteOrder = (order) => ({ type: DELETE_ORDER, orderToDelete: order });
+const updateOrder = (order) => ({ type: UPDATE_ORDER, orderToUpdate: order });
 
 /* ------------       DISPATCHERS     ------------------ */
 export const fetchOrders = () => dispatch => {
-        axios.get('/api/orders')
-        .then(res => dispatch(getOrders(res.data)))
-        .catch(err => console.error('Fetching orders unsuccessful', err))
+  axios.get('/api/orders')
+    .then(res => dispatch(getOrders(res.data)))
+    .catch(err => console.error('Fetching orders unsuccessful', err))
 }
 
 export const fetchOrder = (id) => dispatch => {
-    axios.get(`/api/order/${id}`)
-        .then(res => dispatch(getOrder(res.data)))
-        .catch(err => console.error('Fetching order unsuccessful', err))
+  axios.get(`/api/order/${id}`)
+    .then(res => dispatch(getOrder(res.data)))
+    .catch(err => console.error('Fetching order unsuccessful', err))
 }
 
 export const addOrder = order => dispatch => {
-    axios.post('/api/orders', order)
-        .then(res => dispatch(createOrder(res.data)))
-        .catch(err => console.error(`Creating order: ${order} unsuccessful`, err))
+  axios.post('/api/orders', order)
+    .then(res => dispatch(createOrder(res.data)))
+    .catch(err => console.error(`Creating order: ${order} unsuccessful`, err))
 }
 
 export const changeOrder = (id, order) => dispatch => {
-    axios.put(`/api/orders/${id}`, order)
-        .then(res => dispatch(updateOrder(res.data)))
-        .catch(err => console.error(`Updating order: ${order} unsuccessful`, err))
+  axios.put(`/api/orders/${id}`, order)
+    .then(res => dispatch(updateOrder(res.data)))
+    .catch(err => console.error(`Updating order: ${order} unsuccessful`, err))
 }
 
 export const removeOrder = id => dispatch => {
-  dispatch(deleteOrder(id));
   axios.delete(`/api/stories/${id}`)
-       .catch(err => console.error(`Removing order: ${id} unsuccessful`, err));
+    .then(res => dispatch(deleteOrder(res.data)))
+    .catch(err => console.error(`Removing order: ${id} unsuccessful`, err));
 }
--- app/actions/reviews.js
+++ app/actions/reviews.js
@@ -6,25 +6,25 @@ export const CREATE_REVIEWS = 'CREATE_REVIEWS';
 export const DELETE_REVIEWS = 'DELETE_REVIEWS';
 
 /* ------------   ACTION CREATORS     ------------------ */
-const getReviews = (reviews) => ({ type: GET_REVIEWS, receivedReviews: reviews});
-const createReview = (reviews) => ({ type: CREATE_REVIEWS, reviewsToCreate: reviews});
-const deleteReview = (reviews) => ({ type: DELETE_REVIEWS, reviewsToDelete: reviews});
+const getReviews = (reviews) => ({ type: GET_REVIEWS, receivedReviews: reviews })
+const createReview = (reviews) => ({ type: CREATE_REVIEWS, reviewsToCreate: reviews })
+const deleteReview = (reviews) => ({ type: DELETE_REVIEWS, reviewsToDelete: reviews })
 
 /* ------------       DISPATCHERS     ------------------ */
 export const fetchReviews = () => dispatch => {
-        axios.get('/api/reviews')
-        .then(res => dispatch(getReviews(res.data)))
-        .catch(err => console.error('Fetching reviews unsuccessful', err))
+  axios.get('/api/reviews')
+    .then(res => dispatch(getReviews(res.data)))
+    .catch(err => console.error('Fetching reviews unsuccessful', err))
 }
 
 export const addReview = reviews => dispatch => {
-    axios.post('/api/reviewss', reviews)
-        .then(res => dispatch(createReview(res.data)))
-        .catch(err => console.error(`Creating reviews: ${reviews} unsuccessful`, err))
+  axios.post('/api/reviewss', reviews)
+    .then(res => dispatch(createReview(res.data)))
+    .catch(err => console.error(`Creating reviews: ${reviews} unsuccessful`, err))
 }
 
 export const removeReview = id => dispatch => {
-  dispatch(deleteReview(id));
   axios.delete(`/api/stories/${id}`)
-       .catch(err => console.error(`Removing review: ${id} unsuccessful`, err));
+    .then(res => dispatch(deleteReview(res.data)))
+    .catch(err => console.error(`Removing review: ${id} unsuccessful`, err));
 }
