bb1b8e8fe111ba6361f2bb43153f6da993f1f6db
Jessica Perina <jperina@gmail.com>
2017-03-05 18:21:25 -0500

    refactor(nav): refactor for material-ui

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,21 +1,31 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+
+const style = {
+  'margin-right': '10px'
+}
 
 export default class Navbar extends Component {
   render () {
     console.log(this.props)
     const categories = this.props.categories;
     return (
-        <nav className ="navbar navbar-default">
-          <div className ="container-fluid">
+      <nav className ="navbar navbar-default">
+        <Toolbar>
+          <ToolbarGroup>
+            <div className ="container-fluid">
               <ul className="nav navbar-nav">
-                <li><Link to='/products'>All</Link></li>
+                <RaisedButton style={style}><Link to='/products'>All</Link></RaisedButton>
               {categories && categories.map((category) => {
-                return <li key={category.id}><Link to="#">{category.name}</Link></li>
+                return <RaisedButton style={style} key={category.id}><Link to="#">{category.name}</Link></RaisedButton>
               })}
               </ul>
             </div>
-        </nav>
+          </ToolbarGroup>
+        </Toolbar>
+      </nav>
     )
   }
 }
