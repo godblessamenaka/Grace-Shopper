164b5ff7082f846dc4a58fc746f7461cf7e2629c
Jessica Perina <jperina@gmail.com>
2017-03-06 18:17:33 -0500

    feat(header): switch login component to container

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import RaisedButton from 'material-ui/RaisedButton';
-import Login from '../components/Login'
+import LoginContainer from '../containers/LoginContainer'
 import NavbarContainer from '../containers/NavbarContainer'
 
 const buttonStyle = {
@@ -19,7 +19,7 @@ export default class Header extends Component {
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav">
               <ul className="nav navbar-nav navbar-right">
-                <li><Login /></li>
+                <li><LoginContainer /></li>
                 <li><RaisedButton label="Cart" style={buttonStyle} /></li>
               </ul>
               </ul>
