2c011fe42dd5fa990591f2ae716434835daa3418
Menaka Sampath <menakasampath@gmail.com>
2017-03-06 11:28:48 -0500

    fix(reviews): fixed actions so no duplicate code

--- app/actions/reviews.js
+++ app/actions/reviews.js
@@ -6,24 +6,6 @@ export const GET_REVIEWS = 'GET_REVIEWS';
 export const CREATE_REVIEWS = 'CREATE_REVIEWS';
 export const DELETE_REVIEWS = 'DELETE_REVIEWS';
 
-export const getReviews = (reviews) => ({ type: GET_REVIEWS, receivedReviews: reviews});
-export const createReviews = (reviews) => ({ type: CREATE_REVIEWS, reviewsToCreate: reviews});
-export const deleteReviews = (reviews) => ({ type: DELETE_REVIEWS, reviewsToDelete: reviews});
-
-
-//thunk action creators
-export const fetchReviews = () => {
-    return (dispatch) => {
-        axios.get('/api/reviews')
-        .then((res) => {
-       	  dispatch(getReviews(res.data));
-       	})
-        .catch(function (err) {
-            console.error(err);
-        });
-    };
-};
-
 /* ------------   ACTION CREATORS     ------------------ */
 const getReviews = (reviews) => ({ type: GET_REVIEWS, receivedReviews: reviews })
 const createReview = (reviews) => ({ type: CREATE_REVIEWS, reviewsToCreate: reviews })
