78096a98959041d5e5b9537e11a4eb31e0ef1923
Joe Angelino <angelinojoe@gmail.com>
2017-03-06 11:14:36 -0500

    Update reviews.js

--- app/actions/reviews.js
+++ app/actions/reviews.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 
 /* -----------------    ACTIONS     ------------------ */
+export const GET_REVIEWS = 'GET_REVIEWS';
+export const CREATE_REVIEWS = 'CREATE_REVIEWS';
+export const DELETE_REVIEWS = 'DELETE_REVIEWS';
 
 export const getReviews = (reviews) => ({ type: GET_REVIEWS, receivedReviews: reviews});
 export const createReviews = (reviews) => ({ type: CREATE_REVIEWS, reviewsToCreate: reviews});
