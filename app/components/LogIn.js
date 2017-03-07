3890709366556210c70f565ed502cea9c146694b
Jessica Perina <jperina@gmail.com>
2017-03-07 14:48:35 -0500

    feat(login): update login to dispatch props

--- app/components/LogIn.js
+++ app/components/LogIn.js
@@ -4,6 +4,7 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import { addUser } from '../actions/users'
+import {login} from '../reducers/auth'
 
 /**
  * Dialog with action buttons. The actions are passed in as an array of React objects,
@@ -48,16 +49,17 @@ export default class Login extends React.Component {
   }
 
   state = {
-    open: false,
-    value: 'signIn'
+    open: false
   };
+
+
 
   handleOpen = () => {
-    this.setState({open: true, value: 'signIn'});
+    this.setState({open: true});
   };
 
-    handleOpenRegister = () => {
-    this.setState({open: true, value: 'register'});
+  handleOpenRegister = () => {
+    this.setState({open: true});
   };
 
   handleClose = () => {
@@ -71,6 +73,7 @@ export default class Login extends React.Component {
   }
 
   render() {
+    console.log(this.props);
     const actions = [
       <RaisedButton
         label="join with Google"
@@ -97,7 +100,8 @@ export default class Login extends React.Component {
         label="Submit"
         primary={true}
         keyboardFocused={true}
-        onTouchTap={this.loginSubmit}
+        // onTouchTap={this.onLoginSubmit}
+        type="submit"
       />
     ];
 
@@ -120,10 +124,10 @@ export default class Login extends React.Component {
             <Tab label="Sign In" value="signIn">
               <div style={styles.form}>
                 <h2 style={styles.headline}>Already Have An Account?</h2>
-                <form action="">
-                  
-                  <input style={styles.input} type="text" placeholder="Email" />
-                  <input style={styles.input} type="text" placeholder="Password" />
+                <form onSubmit={this.onLoginSubmit}>
+                  <input style={styles.input} name="email" type="email" placeholder="Email" />
+                  <input style={styles.input} type="password" placeholder="Password" />
+                  <input type="submit" />
                 </form>
               </div>
             </Tab>
@@ -135,17 +139,21 @@ export default class Login extends React.Component {
                   <input style={styles.input} type="text" name="last" placeholder="Last Name" />
                   <input style={styles.input} type="email" name="field2" placeholder="Email" />
                   <input style={styles.input} type="password" name="psw" placeholder="Password" />
-                  <input style={styles.input} type="password" name="psw" placeholder="Re-enter Password" />
+                  <input style={styles.input} type="re-password" name="psw" placeholder="Re-enter Password" />
                 </form>
               </div>
             </Tab>
           </Tabs>
+          <div style={{ textAlign: 'right', padding: 8, margin: '24px -24px -24px -24px' }}>
+            {actions}
+          </div>
         </Dialog>
       </div>
     );
   }
 
   onLoginSubmit(event) {
+    console.log(event.target)
     event.preventDefault();
     const credentials = {
       email: event.target.email.value,
