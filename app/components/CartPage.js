561526254ffb9dabe7b2d948b19f7cc83770569b
Drew Slattery <drewslatts@gmail.com>
2017-03-07 23:52:58 -0500

    feat(cartpage)now renders cart on state

--- app/components/CartPage.js
+++ app/components/CartPage.js
@@ -13,42 +13,17 @@ import RaisedButton from 'material-ui/RaisedButton';
 //   },
 // };
 
-// const dummyCartProducts = [
-//   { name: 'Love Potion #9',
-//     image: 'lovepotion9.jpg',
-//     price: 300.00,
-//     qty: 1
-//   },
-
-//   { name: 'Love Potion #10',
-//     image: 'lovepotion10.jpg',
-//     price: 220.00,
-//     qty: 1
-//   },
-//   { name: 'Love Potion #11',
-//     image: 'lovepotion11.jpg',
-//     price: 220.00,
-//     qty: 1
-
-//   },
-//   { name: 'Felix Felicis',
-//     image: 'felixfelicis.jpg',
-//     price: 220.00,
-//     qty: 3
-//   }
-// ];
-
 const CartPage = (props) => {
-   const productsInCart = props.productsInCart;
-   console.log('here are the contents of your cart: ', productsInCart)
-   const RowsFromProducts = productsInCart.map(function(product){
+  const productsInCart = props.productsInCart;
+  console.log('here are the contents of your cart: ', productsInCart)
+  const RowsFromProducts = productsInCart.map(function(product){
     return (
-      <TableRow key>
-            <TableRowColumn>{product.image}</TableRowColumn>
+      <TableRow key={product.id}>
+            <TableRowColumn><img src={'/images/' + product.image} /> </TableRowColumn>
             <TableRowColumn>{product.name}</TableRowColumn>
             <TableRowColumn>{product.price}</TableRowColumn>
-            <TableRowColumn>{product.qty}</TableRowColumn>
-            <TableRowColumn>{ (product.qty) * (product.price) }</TableRowColumn>
+            <TableRowColumn>{product.quantity}</TableRowColumn>
+            <TableRowColumn>{ (product.quantity) * (product.price) }</TableRowColumn>
       </TableRow>
   );
 });
@@ -68,7 +43,7 @@ const CartPage = (props) => {
         {RowsFromProducts}
         </TableBody>
       </Table>
-      {productsInCart.length===0 ? <RaisedButton label="Keep Shopping" fullWidth={true} /> : <RaisedButton label="Checkout" fullWidth={true} />} 
+      {productsInCart.length === 0 ? <RaisedButton label="Keep Shopping" fullWidth={true} /> : <RaisedButton label="Checkout" fullWidth={true} />}
       </div>
 
     )
